409bc83c117a7a9c7635dd4e3af433ade8d9f8a9
Astek27 <formov.efim@yandex.ru>
2026-04-15 13:22:08 +0300

    Add all tests

--- tests/test_auth_handlers.py
+++ tests/test_auth_handlers.py
@@ -1,13 +1,31 @@
+import base64
 from fastapi.testclient import TestClient
 import pytest
 from app.main import app
 
 
 BASE_URL = "http://localhost:8000"
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+
+@pytest.fixture
+def auth_headers():
+    credentials = f"{USERNAME}:{PASSWORD}"
+    encoded = base64.b64encode(credentials.encode()).decode()
+    return {'Authorization': f'Basic {encoded}'}
+
+
+@pytest.fixture
+def wrong_auth():
+    credentials = f"wrongusername:wrongpassword"
+    encoded = base64.b64encode(credentials.encode()).decode()
+    return {'Authorization': f'Basic {encoded}'}
 
 
 def test_root_endpoint(client):
@@ -28,4 +46,26 @@ def test_public_endpoint(client):
 def test_private_endpoint_no_auth(client):
     response = client.get('/private')
     assert response.status_code == 401
-    # assert response.headers
+    assert "WWW-Authenticate" in response.headers
+    assert response.headers['WWW-Authenticate'] == 'Basic'
+
+
+def test_private_endpoint_wrong_auth(client, wrong_auth):
+    response = client.get('/private', headers=wrong_auth)
+    assert response.status_code == 401
+    assert "WWW-Authenticate" in response.headers
+    assert response.headers['WWW-Authenticate'] == 'Basic'
+
+
+def test_private_endpoint_norm_auth(client, auth_headers):
+    response = client.get('/private', headers=auth_headers)
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize("wrong_headers", [
+    {"Authorization": "Basic invalid"},
+    {"Authorization": "NotBasic asdf"}
+])
+def test_private_endpoint_wrong_headers(client, wrong_headers):
+    response = client.get('/private', headers=wrong_headers)
+    assert response.status_code == 401
